7dc753813da0a4fcb7da830533425f8152c2da00
ParkSeHune <105360156+ParkSeHune@users.noreply.github.com>
2022-10-14 18:59:47 +0900

    Endless

--- Endless Run/Assets/Script/Manager/GameManager.cs
+++ Endless Run/Assets/Script/Manager/GameManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     //GameManager에 인스턴스를 담는 전역변수
-    //이 게임 내에서 GameManager instance 이 instance에 담신 오브젝트만 존재할 수 있다
+    //이 게임 내에서 GameManage instance에 담신 오브젝트만 존재할 수 있다
     public static GameManager instance;
 
     public float speed = 10.0f;
